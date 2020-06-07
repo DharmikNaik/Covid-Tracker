deaf0f5742fc35862c2c19f6e4994178127086a2
dharmiknaik <dharmiknaik@hotmail.com>
2020-06-07 18:14:47 +0530

    Added styling and responsiveness to components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData, fetchCountryDetails } from "./api";
+import imgCovidTracker from "./images/covid_tracker.png";
 
 class App extends React.Component {
   state = {
@@ -23,6 +24,7 @@ class App extends React.Component {
     const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={imgCovidTracker} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker onSelect={this.handleCountryChange} />
         <Chart country={country} data={data} />
