5ccead3b8042278c03b3663064c53290516a3df1
dharmiknaik <dharmiknaik@hotmail.com>
2020-06-07 11:20:43 +0530

    cards completed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import CountUp from "react-countup";
+import cx from "classnames";
 
 const Cards = (props) => {
   console.log(props);
@@ -20,18 +21,21 @@ const Cards = (props) => {
         value: confirmed.value,
         bottomText: "Number of active cases of COVID-19",
         id: 1,
+        classNames: cx(styles.card, styles.infected),
       },
       {
         title: "Recovered",
         value: recovered.value,
         bottomText: "Number of recoveries from COVID-19",
         id: 2,
+        classNames: cx(styles.card, styles.recovered),
       },
       {
         title: "Deaths",
         value: deaths.value,
         bottomText: "Number of deaths caused by COVID-19",
         id: 3,
+        classNames: cx(styles.card, styles.deaths),
       },
     ];
   }
@@ -39,7 +43,14 @@ const Cards = (props) => {
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         {cardsContent.map((card) => (
-          <Grid item component={Card} key={card.id}>
+          <Grid
+            item
+            component={Card}
+            key={card.id}
+            className={card.classNames}
+            xs={12}
+            md={3}
+          >
             <CardContent>
               <Typography color="textSecondary" gutterBottom>
                 {card.title}
@@ -55,9 +66,7 @@ const Cards = (props) => {
               <Typography color="textSecondary">
                 {new Date(lastUpdate).toDateString()}
               </Typography>
-              <Typography variant="body2">
-                Number of active cases of COVID-19
-              </Typography>
+              <Typography variant="body2">{card.bottomText}</Typography>
             </CardContent>
           </Grid>
         ))}
